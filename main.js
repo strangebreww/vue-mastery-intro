0a6f848ec0f77b7f015a55ad149a290ae46b26ca
Alexey Ermakov <strangebrew@yandex.ru>
2023-03-04 19:29:12 +0300

    Add lesson 5

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ const app = Vue.createApp({
             inventory: 9,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
         }
     }
 });
